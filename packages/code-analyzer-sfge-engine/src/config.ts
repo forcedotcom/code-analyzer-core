e2852dc9608a1e93044de4ad09fa9254caab2408
Joshua Feingold <jfeingold35@gmail.com>
2025-03-27 16:30:03 -0500

    @W-17915919@ SFGE Config validation for all fields except java_command

--- packages/code-analyzer-sfge-engine/src/config.ts
+++ packages/code-analyzer-sfge-engine/src/config.ts
@@ -4,21 +4,113 @@ import {
 } from "@salesforce/code-analyzer-engine-api";
 import {getMessage} from "./messages";
 
-// TODO: As soon as we start adding config properties, we can stop ignoring this rule.
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export type SfgeEngineConfig = {
+    disable_limit_reached_violations: boolean;
+    java_max_heap_size?: string;
+    java_thread_count: number;
+    java_thread_timeout: number;
 }
 
 export const DEFAULT_SFGE_ENGINE_CONFIG: SfgeEngineConfig = {
+    disable_limit_reached_violations: false,
+    java_max_heap_size: undefined,
+    java_thread_count: 4,
+    java_thread_timeout: 900000
 };
 
 export const SFGE_ENGINE_CONFIG_DESCRIPTION: ConfigDescription = {
     overview: getMessage('ConfigOverview'),
     fieldDescriptions: {
+        disable_limit_reached_violations: {
+            descriptionText: getMessage('ConfigFieldDescription_disable_limit_reached_violations'),
+            valueType: "boolean",
+            defaultValue: DEFAULT_SFGE_ENGINE_CONFIG.disable_limit_reached_violations
+        },
+        java_max_heap_size: {
+            descriptionText: getMessage('ConfigFieldDescription_java_max_heap_size'),
+            valueType: "string",
+            defaultValue: null
+        },
+        java_thread_count: {
+            descriptionText: getMessage('ConfigFieldDescription_java_thread_count'),
+            valueType: "number",
+            defaultValue: DEFAULT_SFGE_ENGINE_CONFIG.java_thread_count
+        },
+        java_thread_timeout: {
+            descriptionText: getMessage('ConfigFieldDescription_java_thread_timeout'),
+            valueType: "number",
+            defaultValue: DEFAULT_SFGE_ENGINE_CONFIG.java_thread_timeout
+        }
     }
 }
 
+const JAVA_HEAP_SIZE_REGEX: RegExp = /^\d+[kmg]?$/i;
+
 export async function validateAndNormalizeConfig(cve: ConfigValueExtractor): Promise<SfgeEngineConfig> {
-    cve.validateContainsOnlySpecifiedKeys([]);
-    return {};
+    cve.validateContainsOnlySpecifiedKeys(['disable_limit_reached_violations', 'java_max_heap_size', 'java_thread_count', 'java_thread_timeout']);
+    const sfgeConfigValueExtractor: SfgeConfigValueExtractor = new SfgeConfigValueExtractor(cve);
+    return {
+        disable_limit_reached_violations: sfgeConfigValueExtractor.extractBooleanValue('disable_limit_reached_violations'),
+        java_max_heap_size: sfgeConfigValueExtractor.extractJavaMaxHeapSize(),
+        java_thread_count: sfgeConfigValueExtractor.extractNumericValue('java_thread_count'),
+        java_thread_timeout: sfgeConfigValueExtractor.extractNumericValue('java_thread_timeout')
+    };
+}
+
+class SfgeConfigValueExtractor {
+    private readonly delegateExtractor: ConfigValueExtractor;
+
+    public constructor(delegateExtractor: ConfigValueExtractor) {
+        this.delegateExtractor = delegateExtractor;
+    }
+
+    public extractJavaMaxHeapSize(): string | undefined {
+        const javaMaxHeapSize: string | undefined = this.delegateExtractor.extractString('java_max_heap_size', undefined, JAVA_HEAP_SIZE_REGEX);
+
+        if (!javaMaxHeapSize) {
+            return undefined;
+        }
+
+        if (javaMaxHeapSize.toLowerCase().endsWith('g')) {
+            // A value expressed in gigabytes is always fine.
+            return javaMaxHeapSize;
+        } else {
+            const numericPortion: number = parseInt(javaMaxHeapSize);
+            if (numericPortion < expressTwoMegabytesInRelevantUnit(javaMaxHeapSize)) {
+                throw new Error(getMessage(
+                    'InvalidConfigValue',
+                    this.delegateExtractor.getFieldPath('java_max_heap_size'),
+                    getMessage('InsufficientMemorySpecified')
+                ));
+            }
+
+            const isStrictlyNumeric: boolean = /^\d+$/.test(javaMaxHeapSize);
+            if (isStrictlyNumeric && numericPortion % 1024 !== 0) {
+                throw new Error(getMessage(
+                    'InvalidConfigValue',
+                    this.delegateExtractor.getFieldPath('java_max_heap_size'),
+                    getMessage('InvalidMemoryMultiple')
+                ));
+            }
+            return javaMaxHeapSize;
+        }
+    }
+
+    public extractBooleanValue(fieldName: string): boolean {
+        return this.delegateExtractor.extractBoolean(fieldName, DEFAULT_SFGE_ENGINE_CONFIG[fieldName as keyof SfgeEngineConfig] as boolean)!;
+    }
+
+    public extractNumericValue(fieldName: string): number {
+        return this.delegateExtractor.extractNumber(fieldName, DEFAULT_SFGE_ENGINE_CONFIG[fieldName as keyof SfgeEngineConfig] as number)!;
+    }
+}
+
+function expressTwoMegabytesInRelevantUnit(val: string): number {
+    if (val.toLowerCase().endsWith('m')) {
+        return 2;
+    } else if (val.toLowerCase().endsWith('k')) {
+        return 2048; // 2MB === 2048KB
+    } else {
+        return 2 ** 21; // 2MB === 2^21 bytes
+    }
 }
--- packages/code-analyzer-sfge-engine/test/plugin.test.ts
+++ packages/code-analyzer-sfge-engine/test/plugin.test.ts
@@ -1,4 +1,5 @@
 import {
+    ConfigObject,
     ConfigValueExtractor,
     EnginePluginV1,
     getMessageFromCatalog,
@@ -44,14 +45,180 @@ describe('SfgeEnginePlugin', () => {
             const cve: ConfigValueExtractor = new ConfigValueExtractor({invalidField: 2}, 'engines.sfge');
             await expect(plugin.createEngineConfig('sfge', cve)).rejects.toThrow(
                 getMessageFromCatalog(SHARED_MESSAGE_CATALOG, 'ConfigObjectContainsInvalidKey', 'engines.sfge', 'invalidField',
-                    '[]'));
+                    '["disable_limit_reached_violations","java_max_heap_size","java_thread_count","java_thread_timeout"]'));
         });
 
-        // SFGE currently has no configuration properties. When it adds some, this test will need to be replaced with
-        // tests that actually validate the config.
-        it('When given an empty raw config, the config is accepted without change', async () => {
+        it('When given an empty raw config, the correct defaults are returned', async () => {
             const resolvedConfig: SfgeEngineConfig = await plugin.createEngineConfig('sfge', new ConfigValueExtractor({}, 'engines.sfge')) as SfgeEngineConfig;
-            expect(resolvedConfig).toEqual({});
+            // This test assumes that all environments that run this test will have JAVA v11+ installed and either has
+            // * the JAVA_HOME environment variable points to the home folder of this java command
+            // * or has the java command is already on the top of the PATH
+            //expect(resolvedConfig.java_command.endsWith('java')).toEqual(true);
+            expect(resolvedConfig).toEqual({
+                //java_command: resolvedConfig.java_command, // We just checked the Java Command above.
+                disable_limit_reached_violations: false,
+                java_max_heap_size: undefined,
+                java_thread_count: 4,
+                java_thread_timeout: 900000
+            });
+        });
+
+//        describe(`Validating the pathlike 'java_command' property`, () => {
+//            it.each([
+//                {case: 'Java lookup fails'},
+//                {case: 'Java lookup produces outdated Java version'}
+//            ])('When java_command is absent and $case, an error is thrown', async () => {
+//
+//            });
+//
+//            it.each([
+//                {case: 'invalid'},
+//                {case: 'outdated'}
+//            ])('When provided java_command value is $case, an error is thrown', async () => {
+//
+//            });
+//
+//            it('When provided java_command value is valid and up-to-date, it is used', async () => {
+//
+//            });
+//        });
+
+        describe(`Validating the boolean 'disable_limit_reached_violations' property`, () => {
+            it.each([
+                {val: true},
+                {val: false}
+            ])(`Accepts boolean value $val`, async ({val}) => {
+                const rawConfig: ConfigObject = {
+                    disable_limit_reached_violations: val
+                };
+                const configValueExtractor: ConfigValueExtractor = new ConfigValueExtractor(rawConfig, `engines.sfge`);
+                const resolvedConfig: ConfigObject = await plugin.createEngineConfig('sfge', configValueExtractor);
+                expect(resolvedConfig).toHaveProperty('disable_limit_reached_violations', val);
+            });
+
+            it.each([
+                {val: 'someString', type: 'string'},
+                {val: 1, type: 'number'},
+                {val: ['a', 'b', 'c'], type: 'array'},
+                {val: {a: 1}, type: 'object'}
+            ])(`Rejects non-boolean value: $val`, async ({val, type}) => {
+                const rawConfig: ConfigObject = {
+                    disable_limit_reached_violations: val
+                };
+                const configValueExtractor: ConfigValueExtractor = new ConfigValueExtractor(rawConfig, `engines.sfge`);
+                await expect(plugin.createEngineConfig('sfge', configValueExtractor)).rejects.toThrow(
+                    getMessageFromCatalog(SHARED_MESSAGE_CATALOG, 'ConfigValueMustBeOfType',
+                        'engines.sfge.disable_limit_reached_violations', 'boolean', type)
+                );
+            });
+        });
+
+        describe.each([
+            {prop: 'java_thread_count'},
+            {prop: 'java_thread_timeout'}
+        ])(`Validating the numeric property $prop`, ({prop}) => {
+            it('Accepts numeric value', async () => {
+                const rawConfig: ConfigObject = {
+                    [prop]: 5
+                };
+                const configValueExtractor: ConfigValueExtractor = new ConfigValueExtractor(rawConfig, `engines.sfge`);
+                const resolvedConfig: ConfigObject = await plugin.createEngineConfig('sfge', configValueExtractor);
+                expect(resolvedConfig).toHaveProperty(prop, 5);
+            });
+
+            it.each([
+                {val: 'a', type: 'string'},
+                {val: true, type: 'boolean'},
+                {val: [1, 2, 3], type: 'array'},
+                {val: {a: 1}, type: 'object'}
+            ])('Rejects non-numeric value: $val', async ({val, type}) => {
+                const rawConfig: ConfigObject = {
+                    [prop]: val
+                };
+                const configValueExtractor: ConfigValueExtractor = new ConfigValueExtractor(rawConfig, `engines.sfge`);
+                await expect(plugin.createEngineConfig('sfge', configValueExtractor)).rejects.toThrow(
+                    getMessageFromCatalog(SHARED_MESSAGE_CATALOG, 'ConfigValueMustBeOfType',
+                        `engines.sfge.${prop}`, 'number', type)
+                );
+            });
+        });
+
+        describe(`Validating the formatted string property java_max_heap_size`, () => {
+            it.each([
+                {val: '2097152'}, // Java needs >=2MB to run at all.
+                {val: '2048K'},   // Java needs >=2MB to run at all.
+                {val: '2048k'},   // Java needs >=2MB to run at all.
+                {val: '2M'},      // Java needs >=2MB to run at all.
+                {val: '2m'},      // Java needs >=2MB to run at all.
+                {val: '2098176'}, // Un-suffixed numbers must be increments of 1024.
+                {val: '8193K'},   // Suffixed numbers can be any value, and suffix can be any case.
+                {val: '8193k'},   // Suffixed numbers can be any value, and suffix can be any case.
+                {val: '9M'},      // Suffixed numbers can be any value, and suffix can be any case.
+                {val: '9m'},      // Suffixed numbers can be any value, and suffix can be any case.
+                {val: '2G'},      // Suffixed numbers can be any value, and suffix can be any case.
+                {val: '2g'},      // Suffixed numbers can be any value, and suffix can be any case.
+                {val: '02g'}      // Leading zeroes are weird, but they're not invalid.
+            ])('Accepts valid string input: $val', async ({val}) => {
+                const rawConfig: ConfigObject = {
+                    java_max_heap_size: val
+                };
+                const configValueExtractor: ConfigValueExtractor = new ConfigValueExtractor(rawConfig, 'engines.sfge');
+                const resolvedConfig: ConfigObject = await plugin.createEngineConfig('sfge', configValueExtractor);
+                expect(resolvedConfig).toHaveProperty('java_max_heap_size', val);
+            });
+
+            it.each([
+                {
+                    val: '1024',     // This value is too small.
+                    errMsg: `The 'engines.sfge.java_max_heap_size' configuration value is invalid. It must specify >=2MB`
+                }, {
+                    val: '1024k',    // This value is too small.
+                    errMsg: `The 'engines.sfge.java_max_heap_size' configuration value is invalid. It must specify >=2MB`
+                }, {
+                    val: '1m',       // This value is too small.
+                    errMsg: `The 'engines.sfge.java_max_heap_size' configuration value is invalid. It must specify >=2MB`
+                }, {
+                    val: '8388700',  // This value is large enough, but it's not a multiple of 1024.
+                    errMsg: `The 'engines.sfge.java_max_heap_size' configuration value is invalid. It must be divisible by 1024`
+                }, {
+                    val: '2c',       // This value uses an invalid suffix.
+                    errMsg: `The 'engines.sfge.java_max_heap_size' configuration value is invalid. The string did not match the regular expression pattern: /^\\d+[kmg]?$/i`
+                }, {
+                    val: 'g33',      // This value has the right suffix in the wrong place.
+                    errMsg: `The 'engines.sfge.java_max_heap_size' configuration value is invalid. The string did not match the regular expression pattern: /^\\d+[kmg]?$/i`
+                }, {
+                    val: '3g3',      // This value has extra stuff trailing after the suffix.
+                    errMsg: `The 'engines.sfge.java_max_heap_size' configuration value is invalid. The string did not match the regular expression pattern: /^\\d+[kmg]?$/i`
+                }, {
+                    val: 'g',        // Standalone suffix is invalid.
+                    errMsg: `The 'engines.sfge.java_max_heap_size' configuration value is invalid. The string did not match the regular expression pattern: /^\\d+[kmg]?$/i`
+                }, {
+                    val: 'asdf',     // This value is pure nonsense.
+                    errMsg: `The 'engines.sfge.java_max_heap_size' configuration value is invalid. The string did not match the regular expression pattern: /^\\d+[kmg]?$/i`
+                }
+            ])('Rejects invalid string value: $val', async ({val, errMsg}) => {
+                const rawConfig: ConfigObject = {
+                    java_max_heap_size: val
+                };
+                const configValueExtractor: ConfigValueExtractor = new ConfigValueExtractor(rawConfig, `engines.sfge`);
+                await expect(plugin.createEngineConfig('sfge', configValueExtractor)).rejects.toThrow(errMsg);
+            });
+
+            it.each([
+                {val: 2048, type: 'number'},
+                {val: true, type: 'boolean'},
+                {val: [1, 2, 3], type: 'array'},
+                {val: {a: '1024'}, type: 'object'}
+            ])('Rejects non-string value: $val', async ({val, type}) => {
+                const rawConfig: ConfigObject = {
+                    java_max_heap_size: val
+                };
+                const configValueExtractor: ConfigValueExtractor = new ConfigValueExtractor(rawConfig, `engines.sfge`);
+                await expect(plugin.createEngineConfig('sfge', configValueExtractor)).rejects.toThrow(
+                    getMessageFromCatalog(SHARED_MESSAGE_CATALOG, 'ConfigValueMustBeOfType',
+                        `engines.sfge.java_max_heap_size`, 'string', type)
+                );
+            });
         });
     });
 
