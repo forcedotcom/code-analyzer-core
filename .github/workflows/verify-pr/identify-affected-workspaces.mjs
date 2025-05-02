834c132ad1b9d083760bbc3fdc64f56f53ec4317
Joshua Feingold <jfeingold35@gmail.com>
2025-05-02 09:43:36 -0500

    @W-18396029@ Final feedback

--- .github/workflows/verify-pr/identify-affected-workspaces.mjs
+++ .github/workflows/verify-pr/identify-affected-workspaces.mjs
@@ -53,7 +53,7 @@ function displayList(header, list) {
 }
 
 function readChangedFilesFile(changedFilesFileName) {
-    return fs.readFileSync(path.join(__dirname, '..', '..', '..', changedFilesFileName), 'utf-8').split('\n').map(s => s.trim());
+    return fs.readFileSync(path.join(pathToRoot, changedFilesFileName), 'utf-8').split('\n').map(s => s.trim());
 }
 
 function identifyChangedPackages(changedFiles) {
