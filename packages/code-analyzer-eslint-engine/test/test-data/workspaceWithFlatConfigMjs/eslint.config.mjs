5854bf6e7a05ac6f6b57cdd35e5ff71fcad38d5a
Randi Wilson <randi274@gmail.com>
2025-08-15 11:28:26 -0400

    Fix eslint.config.mjs

--- packages/code-analyzer-eslint-engine/test/test-data/workspaceWithFlatConfigMjs/eslint.config.mjs
+++ packages/code-analyzer-eslint-engine/test/test-data/workspaceWithFlatConfigMjs/eslint.config.mjs
@@ -1,6 +1,11 @@
-export const rules = {
-    "array-callback-return": ["error"],
-    "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
-    "no-useless-backreference": ["warn"],
-    "no-useless-escape": [0]
-};
+export default [
+    {
+        rules: {
+            "array-callback-return": ["error"],
+            "array-callback-return": ["error"],
+            "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+            "no-useless-backreference": ["warn"],
+            "no-useless-escape": [0]
+        },
+    },
+];
